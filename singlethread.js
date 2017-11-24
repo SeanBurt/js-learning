c5b48bacc57dbe4ad9f180ea810f526e0686642e
Lie <mzc_2015@163.com>
2017-11-24 14:02:12 +0800

    Update singlethread.js

--- singlethread.js
+++ singlethread.js
@@ -1,3 +1,4 @@
+//example 1
 setTimeout(()=>{
   console.log('timer start')
 },0)
@@ -11,6 +12,7 @@ new Promise((resolve)=>{
 })
 console.log('code execute end')
 
+//example 2
 setTimeout(()=>{
   setTimeout(()=>{
     console.log('timer 1')
@@ -27,5 +29,28 @@ new Promise((resolve)=>{
   },0)
 }).then(()=>{
   console.log('timer 3')
+})
+console.log('code end')
+
+//example 3
+console.log('code start')
+setTimeout(()=>{
+  setTimeout(()=>{
+    console.log('timer 1')
+  },0)
+},0)
+setTimeout(()=>{
+  setTimeout(()=>{
+    console.log('timer 2')
+  },0)
+})
+new Promise((resolve)=>{
+  console.log('promise start')
+  resolve()
+  console.log('promise end')
+}).then(()=>{
+  setTimeout(()=>{
+    console.log('timer 3')
+  })
 })
 console.log('code end')
