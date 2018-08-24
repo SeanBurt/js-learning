fd02826c7ac7dc70efd79e63295783454af2ba00
lie <mzc_2015@163.com>
2018-08-24 19:14:03 +0800

    add mySetInterval

--- common.js
+++ common.js
@@ -1,8 +1,25 @@
+// 字符串全部替换正则版
 function replaceAll1 (val, str) {
   var regx = new RegExp(`^${str}$`, 'g')
   return val.replace(regx, '')
 }
 
+// 字符串全部替换split版
 function replaceAll2 (val, str) {
   return val.split(str).join('')
+}
+
+// 用setTimeout实现setInterval
+function mySetInterval (callback, duration, count) {
+  function interval () {
+    if (typeof count === 'undefined' || count-- > 0) {
+      setTimeout(interval, duration)
+      try {
+        callback()
+      } catch (ex) {
+        throw ex
+      }
+    }
+  }
+  setTimeout(interval, duration)
 }
