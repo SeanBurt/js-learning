c9a728d163d6a7403bce6e3b8fa05bf0df7ae84b
Lie <mzc_2015@163.com>
2018-10-25 10:49:11 +0800

    函数参数传递
    值类型、引用类型参数传递

--- interview-exercise.js
+++ interview-exercise.js
@@ -1,4 +1,4 @@
-1. typeof instanceof Object.prototype.toString.call()
+// 类型判断: typeof instanceof Object.prototype.toString.call()
 var o = {
   a: 1,
   f: function () {
@@ -6,3 +6,30 @@ var o = {
   }
 }
 console.log(typeof o.a, typeof o.f, typeof o.b, typeof o, typeof null)
+
+// 函数参数传递
+var a = 3;
+function func (a) {
+  a = 10;
+  console.log(a);
+}
+func();
+console.log(a);
+
+var o = {};
+function func1 (o) {
+  o.name = 'mr li';
+}
+console.log(o.name);
+func1(o);
+console.log(o.name);
+
+var o1 = {};
+function func2 (o) {
+  o.name = 'mr li';
+  o = new Object();
+  o.name = 'mr lee';
+}
+console.log(o1.name);
+func1(o1);
+console.log(o1.name);
