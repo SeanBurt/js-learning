cc9e6f1b0ede6aa25275394dd01b82cb882f416d
mazc <mzc_2015@163.com>
2020-03-10 17:08:09 +0800

    types array function assert

--- ts/types.ts
+++ ts/types.ts
@@ -44,7 +44,6 @@ let tom: Person = {
   name: "Tom",
   age: 18
 };
-
 interface ParentPerson {
   readonly id: number;
   name: string;
@@ -56,4 +55,50 @@ let joy: ParentPerson = {
   name: "Joy",
   age: 16,
   gender: "womale"
+}
+
+// 数组的类型
+let fibonacci: number[] = [1, 1, 2, 3, 5];
+let fibonacciArray: Array<number> = [1, 1, 2, 3, 5];
+interface NumberArray {
+  [index: number]: number;
+}
+let fibonacciNumberArray: NumberArray = [1, 1, 2, 3, 5];
+let list: any[] = ['xcatliu', 25, { website: 'http://xcatliu.com' }];
+
+// 函数的类型
+function sum(x: number, y: number): number {
+  return x + y;
+}
+let sumFunc = function(x: number, y: number): number {
+  return x + y;
+}
+let mySum: (x: number, y: number) => number = function(x: number, y: number): number {
+  return x + y;
+}
+function buildName(firstName?: string, lastName: string = "Cat") {
+  return firstName ? `${firstName} ${lastName}` : lastName;
+}
+function push(ary: number[], ...items: Array<number>) {
+  items.forEach(item => {
+    ary.push(item);
+  })
+}
+let a = [];
+push(a, 1, 2, 3);
+function reverse(x: number | string): number | string {
+  if (typeof x === "number") {
+    return Number(x.toString().split("").reverse().join(""));
+  } else if (typeof x === "string") {
+    return x.split("").reverse().join("");
+  }
+}
+
+// 类型断言
+function getLength(something: string | number): number {
+  if ((<string>something).length) {
+    return (<string>something).length;
+  } else {
+    return something.toString().length;
+  }
 }
