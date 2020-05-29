77b45354c1a2dfcfebb5a54326dc1345c3e1bb11
mazc <mzc_2015@163.com>
2020-05-29 15:48:18 +0800

    add select sort

--- sort.js
+++ sort.js
@@ -1,18 +1,42 @@
 /**
  * bubble sort
- * 
+ *
  * @param {array} 等待排序的数组
  * @return {array} 排序产出的数组
  */
-let ary = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
-function bubbleSort(ary) {
-    for(let i=0,l=ary.length; i<l; i++) {
-        for(let j=0,m=ary.length-1-i; j<m; j++) {
-            if(ary[j] > ary[j+1]) {
-                ary[j] = ary[j+1] + (ary[j+1]=ary[j])*0
-            }
-        }
+let ary = [9, 1, 4, 7, 2, 8, 3];
+function bubble(ary) {
+  for (let i = 0, l = ary.length; i < l; i++) {
+    for (let j = 0, m = l - 1 - i; j < m; j++) {
+      if (ary[j] < ary[j + 1]) {
+        ary[j] = ary[j + 1] + 0 * (ary[j + 1] = ary[j]);
+      }
     }
-    return ary
+  }
+  return ary;
 }
-console.log(bubbleSort(ary))
+console.log(bubble(ary));
+
+/**
+ * select sort
+ *
+ * @param {array} 等待排序的数组
+ * @return {array} 排序产出的数组
+ */
+function select(ary) {
+  let minIndex = null;
+  let temp = null;
+  for (let i = 0, l = ary.length; i < l; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < l; j++) {
+      if (ary[j] < ary[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = ary[i];
+    ary[i] = ary[minIndex];
+    ary[minIndex] = temp;
+  }
+  return ary;
+}
+console.log(select(ary));
